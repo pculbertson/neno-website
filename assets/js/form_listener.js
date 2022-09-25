7f4f1f6217d215460654be7cab7de610913f23ce
Preston Culbertson <pculbertson@stanford.edu>
2022-09-25 15:11:20 -0700

    updated js

--- assets/js/form_listener.js
+++ assets/js/form_listener.js
@@ -2,20 +2,15 @@ window.addEventListener("load", function() {
   const form = document.getElementById('contact-form');
   form.addEventListener("submit", function(e) {
     e.preventDefault();
+
     const data = new FormData(form);
     const action = e.target.action;
-
-    alert("made it!")
     fetch(action, {
-      redirect: "follow",
       method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "text/plain;charset=utf-8",
-      },
+      body: data,
     })
     .then(() => {
-      alert("Thanks for signing up!");
+      alert("Success!");
     })
   });
 });
